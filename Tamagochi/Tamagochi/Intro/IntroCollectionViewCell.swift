f93fa72437e432eb962c3ff301e4ef157a1de88d
Joobang <hii5074@gmail.com>
2022-07-22 17:08:10 +0900

    [feat] 다마고치 선택화면 storyboard, CollectionViewController, CollectionViewCell 생성

---
+++ Tamagochi/Tamagochi/Intro/IntroCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  IntroCollectionViewCell.swift
+//  Tamagochi
+//
+//  Created by Joobang Lee on 2022/07/22.
+//
+
+import UIKit
+
+class IntroCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Tamagochi/Tamagochi/Intro/IntroCollectionViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class IntroCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
