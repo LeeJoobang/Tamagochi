da0919270d52613b2cde52b74b0327f96f7c86ca
Joobang <hii5074@gmail.com>
2022-07-22 18:49:41 +0900

    [feat] 다마고치 선택화면(Intro) 네비게이션타이틀 변경o

--- Tamagochi/Tamagochi/Intro/IntroCollectionViewController.swift
+++ Tamagochi/Tamagochi/Intro/IntroCollectionViewController.swift
@@ -4,5 +4,6 @@ class IntroCollectionViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
+        self.navigationItem.title = "다마고치 선택하기"
+    }    
 }
