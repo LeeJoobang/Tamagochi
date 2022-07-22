c9f69fa24f777aeb3bbece5eaa2f655c6e19f927
Joobang <hii5074@gmail.com>
2022-07-22 18:53:06 +0900

    [feat] 선택화면 배경색 변경

--- Tamagochi/Tamagochi/Intro/IntroCollectionViewController.swift
+++ Tamagochi/Tamagochi/Intro/IntroCollectionViewController.swift
@@ -5,5 +5,7 @@ class IntroCollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "다마고치 선택하기"
-    }    
+        self.collectionView.backgroundColor = UIColor(red: 245/255, green: 252/255, blue: 252/255, alpha: 1)
+        
+    }
 }
