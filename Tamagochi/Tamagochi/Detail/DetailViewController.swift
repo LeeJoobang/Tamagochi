74ff506d2fa3c94f8693fda662988617bcd07266
Joobang <hii5074@gmail.com>
2022-07-23 16:13:17 +0900

    [feat] detailView - UILabel layout 설정

--- Tamagochi/Tamagochi/Detail/DetailViewController.swift
+++ Tamagochi/Tamagochi/Detail/DetailViewController.swift
@@ -2,9 +2,12 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var backgroundView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .clear
-
+        backgroundView.backgroundColor = UIColor(red: 245/255, green: 252/255, blue: 252/255, alpha: 1)
+        backgroundView.layer.cornerRadius = 5
     }
 }
